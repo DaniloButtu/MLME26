aebcea9f37fec3060eb44cc0af5a2378a603961c
DaniloButtu <s311216@studenti.polito.it>
2026-05-25 23:59:32 +0200

    add comments to train.py

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import warnings
-# Ignora specificamente il warning relativo a wandb.require
+
+# Ignoriamo il warning relativo a wandb.require
 warnings.filterwarnings("ignore", message=".*wandb.require.*")
-# Aggiungiamo i percorsi al sys.path
+
+# Aggiungo i percorsi al sys.path
 project_root = os.path.dirname(os.path.abspath(__file__))
 if project_root not in sys.path:
     sys.path.append(project_root)
@@ -15,7 +17,7 @@ from lightning.pytorch.cli import LightningCLI
 import torch
 torch.set_float32_matmul_precision('medium')
 
-# Importiamo i moduli in modo che LightningCLI possa trovarli tramite class_path
+# Importo i moduli in modo che LightningCLI possa trovarli tramite class_path
 import eomt.training.mask_classification_semantic
 import eomt.training.mask_classification_panoptic
 import eomt.training.mask_classification_instance
@@ -24,9 +26,7 @@ import eomt.datasets.lightning_data_module
 from eomt.datasets.anomaly_dataset import AnomalyDataModule
 
 def main():
-    """
-    Entry point per il training tramite PyTorch Lightning CLI.
-    """
+    # Entry point per il training tramite PyTorch Lightning CLI.
     cli = LightningCLI(
         save_config_kwargs={"overwrite": True},
         parser_kwargs={"parser_mode": "omegaconf"},
